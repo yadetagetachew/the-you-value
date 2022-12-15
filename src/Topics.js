a1fd7effc8a748584335b2cfd4fc968a3ffbbc06
Yadeta Getachew <86324546+yadetagetachew@users.noreply.github.com>
2022-12-15 13:24:51 -0800

    The You Value first commit with outline

---
+++ src/Topics.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Topics = () => {
+    return(
+        <ul className='topic-list'>
+            <li>Wellbeing</li>
+            <li>Mindset</li>
+            <li>Expansion</li>
+        </ul>
+     
+    );
+}
+
+export default Topics;
